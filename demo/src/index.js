83c8cc1540a14c2c9fe64de18801cd47f965535e
Mark Muskardin <mark.muskardin@gmail.com>
2016-04-04 19:06:31 -0700

    more progress on demo page

--- demo/src/index.js
+++ demo/src/index.js
@@ -17,7 +17,7 @@ let Demo = React.createClass({
         <div className="youtube-icon"></div>
         <div className="headline">
           <h1>react-youtube-autocomplete</h1>
-          <p className="tagline">An auto-suggest search box for Youtube apps built with React</p>
+          <p className="tagline">A React-based auto-suggest search box for Youtube apps</p>
         </div>
         <div className="react-icon"></div>
       </div>
@@ -26,13 +26,30 @@ let Demo = React.createClass({
       <em>Demo</em>
       <br />
       <br />
-      <Component
-				apiKey='AIzaSyAtSE-0lZOKunNlkHt8wDJk9w4GjFL9Fu4'
-				maxResults='20'
-				callback={this.showResults}
-        placeHolder='Search Youtube'
-        className="yada-yada"
-      />
+      <div id="main">
+        <Component
+  				apiKey='AIzaSyAtSE-0lZOKunNlkHt8wDJk9w4GjFL9Fu4'
+  				maxResults='20'
+  				callback={this.showResults}
+          placeHolder='Search Youtube'
+          className="my-class-name"
+        />
+        <br />
+        <br />
+        <br />
+        <em>Install</em>
+        <br />
+        <br />
+        <div className="code-snippet">npm install --save react-youtube-autocomplete</div>
+        <br />
+        <br />
+        <br />
+        <a href="https://github.com/hackingbeauty/react-youtube-autocomplete">Project & Docs on Github</a>
+        <br />
+        <br />
+      </div>
+      <iframe src="https://ghbtns.com/github-btn.html?user=hackingbeauty&repo=react-youtube-autocomplete&type=fork&count=true&size=large" frameBorder="0" scrolling="0" width="114px" height="30px"></iframe>
+      <iframe src="https://ghbtns.com/github-btn.html?user=hackingbeauty&repo=react-youtube-autocomplete&type=star&count=true&size=large" frameBorder="0" scrolling="0" width="111px" height="30px"></iframe>
     </div>
   }
 })
